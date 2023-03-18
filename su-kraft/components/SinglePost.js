d40e32286bc38b52d45bf005eb32402d85e50562
Nemanja Mitric <nemanjamitric01@gmail.com>
2023-03-18 17:22:35 +0100

    2

--- su-kraft/components/SinglePost.js
+++ su-kraft/components/SinglePost.js
@@ -1,8 +1,10 @@
 import {Video} from "expo-av";
 import {forwardRef, useEffect, useImperativeHandle, useRef} from "react";
-import {Dimensions} from "react-native";
+import {Dimensions, TouchableWithoutFeedback, View} from "react-native";
+import AvatarImage from "react-native-paper/src/components/Avatar/AvatarImage";
+import {IconButton, Text} from "react-native-paper";
 
-const SinglePost = forwardRef ((props, parentRef) => {
+const SinglePost = forwardRef((props, parentRef) => {
     const ref = useRef(null);
     useImperativeHandle(parentRef, () => ({
         play,
@@ -17,13 +19,13 @@ const SinglePost = forwardRef ((props, parentRef) => {
     }, []);
 
     const play = async () => {
-        if (ref.current == null){
+        if (ref.current == null) {
             return;
         }
 
         const status = await ref.current.getStatusAsync();
 
-        if (status?.isPlaying){
+        if (status?.isPlaying) {
             return;
         }
         try {
@@ -34,13 +36,13 @@ const SinglePost = forwardRef ((props, parentRef) => {
     }
 
     const stop = async () => {
-        if (ref.current == null){
+        if (ref.current == null) {
             return;
         }
 
         const status = await ref.current.getStatusAsync();
 
-        if (!status?.isPlaying){
+        if (!status?.isPlaying) {
             return;
         }
         try {
@@ -52,7 +54,7 @@ const SinglePost = forwardRef ((props, parentRef) => {
 
     const unload = async () => {
         console.log("unload")
-        if (ref.current == null){
+        if (ref.current == null) {
             return;
         }
         try {
@@ -63,17 +65,59 @@ const SinglePost = forwardRef ((props, parentRef) => {
     }
 
     return (
-        <Video
-            ref={ref}
-            videoStyle={{height: Dimensions.get("window").height - 80}}
-            style={{height: Dimensions.get("window").height - 80}}
-            posterStyle={{height: Dimensions.get("window").height - 80}}
-            resizeMode="cover"
-            isLooping={true}
-            source={{
-                uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+        <TouchableWithoutFeedback
+            onPress={async () => {
+                const status = await ref.current.getStatusAsync();
+
+                if (status?.isPlaying) {
+                    console.log("STOP")
+                    stop();
+                } else {
+                    console.log("PLAY")
+                    play();
+                }
             }}
-        />
+            style={{position: 'relative'}}
+        >
+            <>
+                <Video
+                    ref={ref}
+                    videoStyle={{height: Dimensions.get("window").height - 80}}
+                    style={{height: Dimensions.get("window").height - 80}}
+                    posterStyle={{height: Dimensions.get("window").height - 80}}
+                    resizeMode="cover"
+                    isLooping={true}
+                    source={{
+                        uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+                    }}
+                />
+                <View style={{position: 'absolute', padding: 20, width: Dimensions.get("window").width, height: 200, backgroundColor: 'rgba(0,0,0,0.25)', right: 0, bottom: 0}}>
+                    <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+                        <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+                            <AvatarImage size={40} source={require('../assets/People/Person1.png')} />
+                            <Text style={{marginLeft: 10, color: "#fff"}} variant='titleLarge'>@username</Text>
+                        </View>
+                        <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+                            <IconButton
+                                icon="heart"
+                                size={22}
+                                mode="contained-tonal"
+                                onPress={() => console.log('Pressed')}
+                            />
+                            <IconButton
+                                icon="comment"
+                                size={22}
+                                mode="contained-tonal"
+                                onPress={() => console.log('Pressed')}
+                            />
+                        </View>
+                    </View>
+                    <View>
+                        <Text numberOfLines={4} style={{color: '#fff'}} variant='bodyLarge'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium consectetur eligendi facilis id iure labore numquam saepe temporibus! Accusantium aliquam aliquid animi beatae debitis dignissimos doloribus ducimus ea eaque earum eveniet excepturi id iste iusto modi neque nihil nobis non nulla numquam odio perferendis perspiciatis porro, quaerat quam qui quia, sed similique sit soluta tempora tempore temporibus ut veritatis, vitae voluptate voluptatem? Consequuntur laudantium mollitia quia quo sed sint sit.</Text>
+                    </View>
+                </View>
+            </>
+        </TouchableWithoutFeedback>
     )
 })
 
